3c576ded6786a3a24fe9d6d4d07cb8ebe6d9b9a7
MikeHardIce <mikehardice@gmail.com>
2021-11-18 16:45:18 +0000

    fixed little bug when redoing the game, where the timer from the previous game would still run

--- src/two_minutes/core.clj
+++ src/two_minutes/core.clj
@@ -11,6 +11,12 @@
   (gui/update! "back" [:events :mouse-clicked] (fn [_]
                                                  (back-handler #(gui/remove-group! group))))))
 
+(defn update-avion!
+  [avion-name height]
+  (when-let [avion (gui/find-by-name "avion")]
+    (when (not= (-> avion :y) height)
+      (gui/update! avion-name [:args :y] height))))
+
 (defn game-screen
   [clear-screen back difficulty]
   (clear-screen)
@@ -18,34 +24,36 @@
         height (atom 300)]
     (gui/label! "timer" "2:00" {:x 800 :y 50 :width 200 :font-size 24 :font-style [:bold] :group "game-screen"})
     (gui/button! "avion" "(  0^0)" {:x 50 :y @height :width 50 :height 25 :color [:red :white] :group "game-screen"})
-    (gui/label! "exercise" (:representation @current-exercise) {:x 375 :y 200 :width 300 :font-size 24 :font-style [:bold] :group "game-screen"})
+    (gui/label! "exercise" (:representation @current-exercise) {:x 375 :y 50 :width 300 :font-size 24 :font-style [:bold] :group "game-screen"})
     (gui/input! "result" "" {:x 375 :y 800 :width 300 :color [:white :black] :selected? true :font-size 16 :group "game-screen"})
     (create-back-button "game-screen" back 900)
     (gui/update! "result" [:events :key-pressed] (fn [wdg key-code]
                                                    (when (= key-code :enter)
                                                      (try
-                                                       (when-let [result (-> wdg :value Integer/parseInt)]
-                                                         (if (= result (:result @current-exercise))
-                                                           (do
-                                                             (reset! current-exercise (game/generate-exercise difficulty))
-                                                             (gui/update! "exercise" :value (:representation @current-exercise))
-                                                             (gui/update! "avion" [:args :y] (swap! height - 5)))
-                                                           (gui/update! "avion" [:args :y] (swap! height + 5)))
+                                                       (let [result (-> wdg :value Integer/parseInt)
+                                                             new-height (if (= result (:result @current-exercise))
+                                                                          (do
+                                                                            (reset! current-exercise (game/generate-exercise difficulty))
+                                                                            (gui/update! "exercise" :value (:representation @current-exercise))
+                                                                            (swap! height - 5))
+                                                                          (swap! height + 5))]
+                                                         (update-avion! "avion" new-height)
                                                          (gui/update! "result" :value ""))
                                                        (catch Exception e
-                                                         (println (.getMessage e))))))))
-  
-  (go-loop [minutes 2
-            seconds 0]
-    (<! (timeout 1000))
-    (gui/update! "timer" :value (str (format "%2d" minutes)
-                                     ":"
-                                     (format "%02d" seconds)))
-    (when (and (= minutes 0) (= seconds 30))
-      (gui/update! "timer" [:args :color] [:red]))
-    (when (> (+ minutes seconds) 0)
-      (recur (if (= seconds 0) (dec minutes) minutes)
-             (if (= seconds 0) 59 (dec seconds))))))
+                                                         (println (.getMessage e)))))))
+    
+    (go-loop [minutes 2
+              seconds 0]
+      (<! (timeout 1000))
+      (gui/update! "timer" :value (str (format "%2d" minutes)
+                                       ":"
+                                       (format "%02d" seconds)))
+      (when (update-avion! "avion" (swap! height + 4))
+        (when (and (= minutes 0) (= seconds 30))
+          (gui/update! "timer" [:args :color] [:red]))
+        (when (> (+ minutes seconds) 0)
+          (recur (if (= seconds 0) (dec minutes) minutes)
+                 (if (= seconds 0) 59 (dec seconds))))))))
 
 (defn difficulties
   [clear-screen back]
